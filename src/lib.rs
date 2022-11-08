842d702ad0e9b7cc80592504af7425b094905e42
CeNiEi <ryujinjakka216@gmail.com>
2022-11-08 13:37:09 +0530

    add libc

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,9 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(deref_nullptr)]
 
+pub use self::bindings::*;
+
+#[cfg(feature = "bindgen")]
+mod bindings;
