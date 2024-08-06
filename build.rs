602bf3541c8ac0def2c84982888e7e0b7f900d50
Saul <47524932+spector-9@users.noreply.github.com>
2024-08-06 21:37:57 +0530

    Fix: Disable simd for emscripten target

--- build.rs
+++ build.rs
@@ -1,10 +1,16 @@
 use anyhow::Result;
 use cmake::Config;
 use std::{env, path::PathBuf};
+use tap::prelude::*;
 
 const MANIFEST_DIR: &str = env!("CARGO_MANIFEST_DIR");
 
 pub fn main() -> Result<()> {
+    let os = std::env::var("CARGO_CFG_TARGET_OS").expect("CARGO_CFG_TARGET_OS not set");
+
+    if os == "emscripten" {
+        println!("cargo:rustc-link-arg=--no-entry");
+    }
     let vendor = PathBuf::from(MANIFEST_DIR).join("vendor");
     anyhow::ensure!(
         vendor.exists(),
@@ -13,6 +19,13 @@ pub fn main() -> Result<()> {
     let dst = Config::new(vendor)
         // OFF
         .define("WITH_JAVA", "OFF")
+        .pipe(|x| {
+            if os == "emscripten" {
+                x.define("WITH_SIMD", "OFF")
+            } else {
+                x
+            }
+        })
         .define("ENABLE_SHARED", "OFF")
         .define("WITH_TURBOJPEG", "OFF")
         // ON
@@ -21,7 +34,9 @@ pub fn main() -> Result<()> {
         // .build_arg(format!("-j{}", std::thread::available_parallelism()?.get()))
         .build();
 
-    let lib_folder = if cfg!(target_os = "linux") {
+    let lib_folder = if cfg!(target_os = "linux")
+        && std::env::var("CARGO_CFG_TARGET_POINTER_WIDTH").expect("CARGO_CFG_TARGET_FAMILY") == "64"
+    {
         "lib64"
     } else {
         "lib"
